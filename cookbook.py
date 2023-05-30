b8c956f5f2b1e6a058b17a5edcfca3cf51e42c0c
Irina Zhelovatykh <skyy.isoo@gmail.com>
2023-05-30 14:39:46 +0500

    Added get_shop_list_by_dishes func

--- cookbook.py
+++ cookbook.py
@@ -17,3 +17,23 @@ with open('recipes.txt', 'rt', encoding='utf8') as rec_file:
     res = json.dumps(cook_book, indent=2, ensure_ascii=False)
     print(f'cook_book = {res}')
 
+def get_shop_list_by_dishes(dishes, person_count):
+    result = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for ingredients in cook_book[dish]:
+                if ingredients['ingredient_name'] in result:
+                    result[ingredients['ingredient_name']]['quantity'] += int(ingredients['quantity']) * person_count
+                else:
+                    result[ingredients['ingredient_name']] = {
+                        'quantity': int(ingredients['quantity']) * person_count,
+                        'measure': ingredients['measure']
+                    }
+        else:
+            print('Блюдо не найдено')
+    res = json.dumps(result, indent=2, ensure_ascii=False)
+    print(f'\nДля приготовления блюд: {", ".join(dishes)} \nНа количество человек: {person_count} \nПотребуется: {res}')
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+get_shop_list_by_dishes(['Запеченный картофель', 'Картофельная запеканка'], 2)
+get_shop_list_by_dishes(['Омлет', 'Сырники'], 4)
